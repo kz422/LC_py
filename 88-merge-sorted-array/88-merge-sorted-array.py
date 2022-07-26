bf710a93bbaf2cc29ed0812c451043cbea7d2987
pino <p.inoue.p@gmail.com>
2022-07-27 05:28:00 +0900

    Time: 49 ms (69.64%), Space: 14.1 MB (5.43%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for i in range(len(nums2)):
+            print(nums1[i+m], nums2[i])
+            nums1[i+m]=nums2[i]
+
+        nums1.sort()
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
