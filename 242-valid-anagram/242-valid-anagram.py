d22d29a093e797fea8a92b2e2f4f8962aa9ffb1a
pino <p.inoue.p@gmail.com>
2022-07-28 23:21:03 +0900

    Time: 31 ms (99.86%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,14 +1,19 @@
+import collections
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         ## model solution
-        return (sorted(list(s)) == sorted(list(t)))
+        # return (sorted(list(s)) == sorted(list(t)))
+        
+        ## model solution 2
+        return collections.Counter(s) == collections.Counter(t)
     
         ## my solution - solved but late
-        if len(s) != len(t):
-            return False
-        l = list(t)
-        for i in range(len(s)):
-            if s[i] in l:
-                l.remove(s[i])
+#         if len(s) != len(t):
+#             return False
+#         l = list(t)
+#         for i in range(len(s)):
+#             if s[i] in l:
+#                 l.remove(s[i])
             
-        return True if not l else False
+#         return True if not l else False
