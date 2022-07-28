0613774eef7501bb49a85432be6876a6a4369be1
pino <p.inoue.p@gmail.com>
2022-07-29 01:32:34 +0900

    Time: 1607 ms (6.97%), Space: 15.5 MB (65.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,18 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        ## model solution
+        nums.sort(key=bool, reverse=True)
+        
+        ## my solution - solved but late
         if len(nums) == 1:
             nums = nums
         for i in range(len(nums)):
             if nums[i] == 0:
                 nums.remove(nums[i])
                 nums.append(0)
-        # for i in range(len(nums)):
-        #     for j in range(len(nums) - 1 - i):
-        #         if nums[j] == 0:
-        #             nums.remove(nums[j])
-        #             nums.append(0)
-        #         if nums[j] > nums[j + 1]:
-        #             nums[j], nums[j + 1] = nums[j + 1], nums[j]
                     
         print(nums)
