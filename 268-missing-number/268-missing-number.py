fb2c3d44d7179ee98871e7fed8b57ce5d38def16
pino <p.inoue.p@gmail.com>
2022-07-29 01:00:15 +0900

    Time: 267 ms (25.66%), Space: 15.2 MB (33.23%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,5 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        ## model solution
+        n = len(nums)
+        return n * (n+1) // 2 - sum(nums)
+    
+        ## my solution - solved but late
         for i in range(len(nums) + 1):
             if i not in nums:
                 return i
