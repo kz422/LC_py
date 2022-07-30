f3fc6590ba7e32daee616eff77837f8cc754ae25
pino <p.inoue.p@gmail.com>
2022-07-31 01:58:28 +0900

    Time: 38 ms (81.59%), Space: 13.8 MB (96.22%) - LeetHub

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -1,5 +1,7 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
+        return word.isupper() or word.islower() or word.istitle()
+    
         u = word.upper()
         l = word.lower()
         d = word[1:]
